2fa0bb8488302ffebf2d6f2330fddb15b4b3339f
Sriram Ramanujam <sriram.reseacher@gmail.com>
2025-03-23 23:33:35 -0700

    sas

--- practice.cpp
+++ practice.cpp
@@ -161,6 +161,19 @@ void peuler_03() {
         }
         _i++; 
     }
+    // Create a structure variable called myStructure
+struct {
+    int myNum;
+    string myString;
+  } myStructure;
+  
+  // Assign values to members of myStructure
+  myStructure.myNum = 1;
+  myStructure.myString = "Hello World!";
+  
+  // Print members of myStructure
+  cout << myStructure.myNum << "\n";
+  cout << myStructure.myString << "\n";
 
 }
  
