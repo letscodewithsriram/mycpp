1767a1803675289fbcd35d6d0006f271039b7917
letscodewithsriram <sriram.researcher@gmail.com>
2025-03-28 14:22:11 -0700

    Date time

--- practice.cpp
+++ practice.cpp
@@ -13,6 +13,7 @@
 # include <sstream>
 # include <string>
 # include <map>
+# include <ctime>
 
 using namespace std;
 
@@ -279,7 +280,29 @@ void excep() {
       }
 
 }
+
+void datatim() {
+    struct tm datetime;
+time_t timestamp;
+
+datetime.tm_year = 2023 - 1900; // Number of years since 1900
+datetime.tm_mon = 12 - 1; // Number of months since January
+datetime.tm_mday = 17;
+datetime.tm_hour = 12;
+datetime.tm_min = 30;
+datetime.tm_sec = 1;
+// Daylight Savings must be specified
+// -1 uses the computer's timezone setting
+datetime.tm_isdst = -1;
+
+timestamp = mktime(&datetime);
+
+cout << ctime(&timestamp);
+}
+
 int main() {
+
+    datatim();
 
     excep();
 
