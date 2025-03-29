6550d69df03138f5e5e529d46c9dcdaaa8cc7ad8
letscodewithsriram <sriram.researcher@gmail.com>
2025-03-28 18:24:39 -0700

    Project Euler #11

--- practice.cpp
+++ practice.cpp
@@ -15,6 +15,7 @@
 # include <map>
 # include <ctime>
 
+
 using namespace std;
 
 void prac_print() {
@@ -300,7 +301,30 @@ timestamp = mktime(&datetime);
 cout << ctime(&timestamp);
 }
 
+void peuler11() {
+
+    int a,b,c;
+    for (a=1; a<=1000; a++)
+      {
+      for (b=1; b<=1000-a; b++)
+        {
+        c=1000-(a+b);
+        if ((a*a+b*b)==c*c)
+       {
+       if (a < b) {
+      cout << "a= " << a << ", b = " << b << ", c= " << c << endl;
+       cout << a*b*c << endl;
+           }
+       }
+        }
+      }
+
+}
+
+
 int main() {
+
+    peuler11();
 
     datatim();
 
